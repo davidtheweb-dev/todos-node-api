38569da97ad5d795ce86e3fb05a142a05abe8dae
davidheweb <davidhermosilladuro@gmail.com>
2022-08-03 13:33:45 +0200

    Adding PATCH and DELETE routes / endpoints

--- models/todo.model.js
+++ models/todo.model.js
@@ -9,14 +9,10 @@ class Todo {
   }
 
   static async getAllTodos() {
-    const todoDocuments = await db
-      .getDb()
-      .collection("todos")
-      .find()
-      .toArray();
+    const todoDocuments = await db.getDb().collection("todos").find().toArray();
 
     return todoDocuments.map(function (todoDocument) {
-      return new Todo(todoDocument._id, todoDocument.text);
+      return new Todo(todoDocument.text, todoDocument._id);
     });
   }
 
